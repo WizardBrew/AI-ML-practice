1d3ddff66cd604b2ee35f9bbb4a435beb0c8213c
WizardBrew <wizardbrew84@gmail.com>
2026-07-12 23:17:31 +0530

    Add local Python practice files

---
+++ Second.py
@@ -0,0 +1,31 @@
+#Variables 
+
+a = 10;  # A Saves Value of 10 -- you can use ; to terminate a statement in python but it is not necessary.
+b = 20  # B Saves Value of 20 
+
+c = a + b;  # C Saves Value of 30 as result of a + b. --good practice to use ; to terminate a statement
+
+print(c);  # Prints Value of C which is 30
+
+'''
+Not valid Variable Names in Python
+
+1. 1a = 10;  # Invalid Variable Name -- cannot start with a number
+2. a b = 10;  # Invalid Variable Name -- cannot contain spaces
+3. a-b = 10;  # Invalid Variable Name -- cannot contain hyphens
+
+'''
+
+
+
+'''
+Cases in programming languages
+
+camelCase = myVariableName;  # First letter of the first word is lowercase, and the first letter of each subsequent word is capitalized.
+PascalCase = MyVariableName;  # First letter of each word is capitalized.
+snake_case = my_variable_name;  # Words are separated by underscores.
+SCREAMING_SNAKE_CASE = MY_VARIABLE_NAME;  # All letters are uppercase, words are separated by underscores.
+
+'''
+
+
--- first.py
+++ first.py
@@ -1,2 +1,14 @@
-print('Hello World\n')
+print('Hello World\n')     
 print('This is a test script.\n')
+
+#commenting out the next line to prevent execution.
+
+
+''' Multiple lines of code can be commented out using triple quotes.
+this is a multi-line comment.
+this will not be executed.'''
+
+"""Same as above, this is also a multi-line comment using triple double quotes.
+This is useful for adding documentation or temporarily disabling code blocks.
+this will not be executed either."""
+
